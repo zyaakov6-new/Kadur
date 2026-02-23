5f203355a44bacb968ebd7c24f956c420f12ea4f
zyaakov6-new <z.yaakov6@gmail.com>
2026-02-23 09:23:45 +0200

    Create colors.ts

---
+++ constants/colors.ts
@@ -0,0 +1,38 @@
+const Colors = {
+  primary: '#0D7C5F',
+  primaryDark: '#065E47',
+  primaryLight: '#E8F5F0',
+  primaryMuted: '#B8DDD0',
+
+  background: '#F5F5F7',
+  surface: '#FFFFFF',
+  surfaceSecondary: '#EBEBF0',
+
+  text: '#1D1D1F',
+  textSecondary: '#6E6E73',
+  textTertiary: '#AEAEB2',
+  textInverse: '#FFFFFF',
+
+  border: '#E5E5EA',
+  borderLight: '#F2F2F7',
+
+  error: '#FF3B30',
+  warning: '#FF9500',
+  success: '#34C759',
+
+  statusOpen: '#34C759',
+  statusFull: '#FF9500',
+  statusClosed: '#8E8E93',
+
+  shadow: '#000000',
+
+  light: {
+    text: '#1D1D1F',
+    background: '#F5F5F7',
+    tint: '#0D7C5F',
+    tabIconDefault: '#AEAEB2',
+    tabIconSelected: '#0D7C5F',
+  },
+};
+
+export default Colors;
